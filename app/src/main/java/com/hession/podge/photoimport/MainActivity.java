24a06f4641c25ef78427834910932868e640ca65
Padraig <C13329991@mydit.ie>
2015-11-06 11:08:17 +0000

    Added photo import functionality

--- app/src/main/java/com/hession/podge/photoimport/MainActivity.java
+++ app/src/main/java/com/hession/podge/photoimport/MainActivity.java
@@ -1,15 +1,31 @@
 package com.hession.podge.photoimport;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.security.Permission;
 
 public class MainActivity extends AppCompatActivity {
+
+    //create a Constant variable to hold the result of the Permission the user chose
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +33,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //int permissionCheck = ContextCompat.checkSelfPermission(MainActivity, Manifest.permission.READ_EXTERNAL_STORAGE);
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE);
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            //should I show an explanation to the user?
+
+
+        } else {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+        }
+
+        //change the intent of the app
+        //Intent.ACTION_PICK indicates that I'm gonna pick media and
+        //MediaStore.Images.Media.EXTERNAL_CONTENT_URI indicates the place I want to get and media from (MediaStore) and the type of media (Images)
+        Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        //later, when I type code for a response I'm gonna look for a returned value of 1
+        startActivityForResult(i, 1);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +62,76 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+//to do something with the selected image, use the onActivityResult method
+//this method gets called whenever I go to an external activity i.e. the Image Picker Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        //requestCode is the code that was sent when the activity was initiated. i.e. 1
+        //RESULT_OK means the user selected an image and didn't cancel the selection
+        //data is the image
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+
+
+            //Uri is the location of the selected image
+            Uri selectedImage = data.getData();
+
+
+            //try and catch is used to ensure the image is a valid image
+            try {
+
+                //convert the image to a bitmap
+                //What i'm saying here is: "Take my image and make a bitmap from it"
+                Bitmap bitmapImage = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+
+                //get the image view
+                ImageView imageView = (ImageView)findViewById(R.id.imageViewId);
+                //set the image bitmap
+                imageView.setImageBitmap(bitmapImage);
+
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+
+    }
+
+    //override the onRequestPermissionsResult method
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE: {
+                //if request is cancelled the result arrays are empty
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    //permission was granted
+                    //do the photo import task you would like to do
+
+                } else {
+
+
+                    //permission denied
+                    //disable the functionality that depends on the permission
+
+                }
+
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 
     @Override
